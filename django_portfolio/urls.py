dee2c795cadf1bee0f285d28c5063febb77fe268
owenthcarey <owenthcarey@gmail.com>
2023-05-13 22:15:30 -0700

    Add accounts app

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path("polls/", include("polls.urls")),
+    path('accounts/', include('accounts.urls')),
     path("admin/", admin.site.urls),
 ]
