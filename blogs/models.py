a86a1728b8eddc32664f5cec3ec93d6e89922cb8
owenthcarey <owenthcarey@gmail.com>
2023-05-15 10:09:10 -0700

    Add blogs app

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,23 @@
+import datetime
+from django.contrib import admin
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    pub_date = models.DateTimeField("date published")
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Blog
 
 
-def blogs(request):
-    return render(request, "blogs/index.html")
+class IndexView(generic.ListView):
+    template_name = "blogs/index.html"
+    context_object_name = "latest_blog_list"
+
+    def get_queryset(self):
+        return Blog.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Blog
+    template_name = "blogs/detail.html"
